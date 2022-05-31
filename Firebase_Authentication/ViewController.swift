26996f105cebc58633f7f591a2f485b31cc23849
Lam Nguyen <lamcnguyen89@gmail.com>
2022-05-31 15:56:35 -0400

    Initial Commit

---
+++ Firebase_Authentication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase_Authentication
+//
+//  Created by Lam Nguyen on 5/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
