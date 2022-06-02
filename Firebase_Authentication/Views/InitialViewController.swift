b29b3bab59e59ff2c2e95faafc08ed5ed9439c64
Lam Nguyen <lamcnguyen89@gmail.com>
2022-06-01 20:25:26 -0400

    Added a background video

--- Firebase_Authentication/Views/InitialViewController.swift
+++ Firebase_Authentication/Views/InitialViewController.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import AVKit
 
 class InitialViewController: UIViewController {
+    
+    var videoPlayer:AVPlayer?
+    
+    var videoPlayerLayer:AVPlayerLayer?
     
     @IBOutlet weak var signUpButton: UIButton!
     @IBOutlet weak var loginButton: UIButton!
@@ -17,7 +22,44 @@ class InitialViewController: UIViewController {
         // Do any additional setup after loading the view.
         setUpElements()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // Set up video in the background
+        setupVideo()
+    }
 
+    func setupVideo() {
+        
+        // Get the path to the resource in the bundle
+        let bundlePath = Bundle.main.path(forResource: "loginbg", ofType: "mp4")
+        
+        guard bundlePath != nil else {
+            return
+        }
+        
+        // Create a URL from it
+        let url = URL(fileURLWithPath: bundlePath!)
+        
+        // Create the video player item
+        let item = AVPlayerItem(url: url)
+        
+        // Create the player
+        videoPlayer = AVPlayer(playerItem: item)
+        
+        // Create the layer
+        videoPlayerLayer = AVPlayerLayer(player: videoPlayer!)
+        
+        // Adjust the size of the frame
+        videoPlayerLayer?.frame = CGRect(x: -self.view.frame.size.width*1.5, y: 0, width: self.view.frame.size.width*4, height: self.view.frame.size.height)
+        
+        view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+        
+        // Add it to the view and play it
+        videoPlayer?.playImmediately(atRate: 0.3)
+        
+    }
+    
     func setUpElements() {
         
         // Style Elements
